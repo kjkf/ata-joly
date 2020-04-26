65b8e9e49edec8a59920d46b37417033f5a32f49
Lola <yakjkf@gmail.com>
2020-04-26 11:54:04 +0600

    menu-lang event

---
+++ src/js/events.js
@@ -0,0 +1,19 @@
+;(function() {
+    const langMenu = document.querySelector('.lang-menu');
+
+    langMenu.addEventListener('click', e => {
+        if (langMenu.classList.contains('selected')) {
+            langMenu.classList.remove('selected');
+        } else {
+            let current = e.target.closest('li');
+            if (current) {
+                let active = langMenu.querySelector('li.active');
+                active.classList.remove('active');
+                current.classList.add('active');
+                langMenu.classList.add('selected');
+            }
+        }
+
+    });
+
+})();
