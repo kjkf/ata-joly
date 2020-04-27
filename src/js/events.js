59de68656b69e60a6067f0d3012532dfaa0a672f
Lola <yakjkf@gmail.com>
2020-04-27 17:32:59 +0600

    mobile header scroll

--- src/js/events.js
+++ src/js/events.js
@@ -3,6 +3,7 @@
     const socialMenu = document.querySelector('.social-menu');
     const headerNav = document.querySelector('.header__nav');
     const btnGumburg = document.querySelector('.btn-gamburg');
+    const headerMobile = document.querySelector('.header-mob');
 
     if (window.innerWidth < 1024) {
         const headerMobileContainer = document.querySelector('.header-mob .container');
@@ -33,6 +34,19 @@
         btnClose.addEventListener('click', e => {
             headerNav.classList.remove('active');
         });
+
+    });
+
+    window.addEventListener('scroll', e => {
+        if (window.innerWidth < 1024) {
+            //console.log(document.documentElement.clientHeight, document.documentElement.scrollHeight);
+            //console.log(pageYOffset);
+            if (pageYOffset === document.documentElement.scrollHeight - document.documentElement.clientHeight) {
+                headerMobile.style.display = 'none';
+            } else {
+                headerMobile.style.display = 'flex';
+            }
+        }
 
     });
 
